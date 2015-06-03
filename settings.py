96c86c0b4746ecbc83b738c8794fcc8b9afc4b6e
Guido Accardo <gaccardo@gmail.com>
2015-06-03 13:46:55 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 BASE_URL = "https://www.buxfer.com/api"
 USER = "gaccardo@gmail.com"
-PASS = "PePe1234"
+PASS = ""
 
 SMTPSERVER = 'smtp.gmail.com'
 SMTPPORT = '587'
@@ -8,7 +8,7 @@ RECIPIENTS = ['gaccardo@gmail.com',
     'dabaravalle@gmail.com']
 SENDER = 'gaccardo@gmail.com'
 EMAIL_USER = 'gaccardo@gmail.com'
-EMAIL_PASS = 'PedroipCop1987GuIdO1987'
+EMAIL_PASS = ''
 TLS = True
 SEND_EMAIL = True
 DEBUG_EMAIL = False
