e1b14fd93c954355f367cc6c7645574ed6b38eac
Guido Accardo <gaccardo@gmail.com>
2015-06-05 12:32:19 -0300

    request can send the report via email by adding email=1 to the qs

--- http_api/api.py
+++ http_api/api.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 
 from buxfer_commands import daemon
 import settings
@@ -12,7 +12,13 @@ app.config.from_object(__name__)
 @app.route('/buxfer/report')
 def get_report():
     d = daemon.BuxferDaemon()
-    d.send_report(http=True)
+
+    import ipdb;ipdb.set_trace()
+
+    if request.args.get('email') == '1':
+        d.send_report(http=False)
+    else:
+        d.send_report(http=True)
 
     report_path = os.path.join(settings.REPORT_TMP, 
         settings.REPORT_NAME)
