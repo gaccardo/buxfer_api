bbdc8a5f02d645b6cb2b5623ad186401660d3e18
Guido Accardo <gaccardo@gmail.com>
2015-06-05 11:58:55 -0300

    http parameters in settings

--- http_server.py
+++ http_server.py
@@ -1,4 +1,6 @@
 from http_api.api import app
+import settings
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host=settings.HTTP_API_LISTEN,
+        port=settings.HTTP_API_PORT)
