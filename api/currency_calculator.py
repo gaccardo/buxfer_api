5082f6c8af19be76f26ba276484cee067a2e8627
Guido Accardo <gaccardo@gmail.com>
2015-06-11 12:29:03 -0300

    dolarblue as source

--- api/currency_calculator.py
+++ api/currency_calculator.py
@@ -1,9 +1,11 @@
 import urllib2
 from htmldom import htmldom
 
+import settings
+
 class CurrencyCalculator( object ):
 
-    def get_dolar(self):
+    def get_dolar_from_argentinadolar(self):
         url = "http://argentinadolar.com/"
 
         site = urllib2.urlopen(url)
@@ -28,8 +30,28 @@ class CurrencyCalculator( object ):
         return {'compra': compra_raw, 'venta': venta_raw,
                 'promedio': promedio, 'real': promedio-0.10}
 
+    def get_dolar_from_dolarblue(self):
+        url = "http://www.preciodolarblue.com.ar/"
 
-# Test
-if __name__ == '__main__':
-    CC = CurrencyCalculator()
-    print CC.get_dolar()
+        site = urllib2.urlopen(url)
+        data = site.read()
+        site.close()
+
+        dom = htmldom.HtmlDom().createDom(data)
+
+        compra_raw = dom.domNodes['td'][3].getText()
+        compra_raw = float(compra_raw)
+
+        venta_raw = dom.domNodes['td'][4].getText()
+        venta_raw = float(venta_raw) 
+
+        promedio = (compra_raw + venta_raw) / 2
+        promedio = float('%.2f' % promedio)
+
+        return {'compra': compra_raw, 'venta': venta_raw,
+                'promedio': promedio, 'real': promedio-0.10}
+
+    def get_dolar(self):
+        dolar = getattr(self, 
+            'get_dolar_from_%s' % settings.DOLAR_SOURCE)
+        return dolar()
